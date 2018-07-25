a8953a29d5ca446d2468442d4f64ba5119a70ab1
Glitch (reddit-for-trello) <none>
2018-07-25 17:20:14 +0000

    ⛲️🚃 Checkpoint ./public/js/getDataFromUrl.js:1131433/241

--- public/js/getDataFromUrl.js
+++ public/js/getDataFromUrl.js
@@ -1,6 +1,14 @@
 const Promise = TrelloPowerUp.Promise
 
 module.exports = (t, url) => {
-  var xhr = new XMLHttpRequest();
-  xhr.get(
+  Promise.try(() => {
+    var xhr = new XMLHttpRequest();
+    var data = {};
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+      data = xhr.response;
+    };
+    xhr.send()
+  });
 };
