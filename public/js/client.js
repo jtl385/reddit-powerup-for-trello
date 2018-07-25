eef1aed9e1503102fe708b6e0a20f95305730f31
Glitch (reddit-for-trello) <none>
2018-07-25 15:19:54 +0000

    🐓🕰 Checkpoint ./public/js/client.js:1131433/164

--- public/js/client.js
+++ public/js/client.js
@@ -5,5 +5,11 @@ var Promise = TrelloPowerUp.Promise;
 var REDDIT_ICON = 'https://png.icons8.com/metro/1600/reddit.png';
 
 TrelloPowerUp.initialize({
+  'card-buttons': function(t, options){
+    return [{
+      'icon': REDDIT_ICON,
+      
+    }]
+  },
   
 });
