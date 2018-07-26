8779f31c4af99bf9d3b14ec0c8ec31ee63de3ded
Glitch (reddit-for-trello) <none>
2018-07-26 14:14:39 +0000

    🏕🐮 Checkpoint ./public/scripts/client.js:1131433/686 ./public/scripts/get-data-from-url.js:1131433/486 ./views/index.html:1131433/49

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,5 @@
 /* global TrelloPowerUp */
 
-const getDataFromUrl = require('./get-data-from-url.js');
-
 var Promise = TrelloPowerUp.Promise;
 
 const REDDIT_ICON = 'https://png.icons8.com/metro/1600/reddit.png';
@@ -19,6 +17,29 @@ const isRedditLink = (url) => {
   
 }
 
+const getDataFromUrl = (t, url) => {
+  var xhr = new XMLHttpRequest();
+  var data = {};
+  xhr.open('GET', url);
+  xhr.responseType = 'json';
+  xhr.onload = () => {
+    if (Array.isArray(xhr.response)){
+      try{ 
+        data = xhr.response[0].data.children[0].data;
+        return data;
+        }
+      catch(err){
+        console.log("Error getting json data");
+        console.error(err);
+      }
+    }
+    else {
+      console.log("Response was not an array");
+    }
+  };
+  xhr.send()
+};
+
 TrelloPowerUp.initialize({
   'card-buttons': (t, options) => {
     return [{
@@ -33,6 +54,9 @@ TrelloPowerUp.initialize({
   },
   
   'attachment-thumbnail': (t, options) => {
+    if (!isRedditLink(options.url)){
+      throw t.notHandled();
+    }
     let data, redditTitle, imageUrl, dataUrl;
     
     dataUrl = options.url + '/.json';
